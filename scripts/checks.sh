9b57b1f937029f05cf66c214cc7510bb727da481
mrmamongo <puper.e@yandex.ru>
2021-03-30 12:39:40 +0300

    single-file client

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -14,7 +14,7 @@ CMAKE_CONFIG_OPTS="-DHUNTER_CONFIGURATION_TYPES=Debug -DCMAKE_BUILD_TYPE=Debug"
 CMAKE_TOOLCHAIN_OPTS="-DCMAKE_TOOLCHAIN_FILE='$(pwd)/tools/polly/sanitize-address-cxx17-pic.cmake'"
 CMAKE_OPTS="$CMAKE_LINKER_OPTS $CMAKE_CONFIG_OPTS $CMAKE_TOOLCHAIN_OPTS"
 cmake -H. -B_builds/sanitize-address-cxx17 $CMAKE_OPTS
-cmake --build _builds/sanitize-address-cxx17
+#cmake --build _builds/sanitize-address-cxx17
 #./_builds/sanitize-address-cxx17/tests
 # thread sanitizer
 #CMAKE_TOOLCHAIN_OPTS="-DCMAKE_TOOLCHAIN_FILE='$(pwd)/tools/polly/sanitize-thread-cxx17-pic.cmake'"
